a36881b75685d9b40bcd497093c5e194ebd90b52
vinh <positivinh@gmail.com>
2025-03-04 18:38:01 +0100

    misc: fix typo

--- web-security-dummy-project/src/main/kotlin/io/positivinh/virtuoso/web/security/dummy/permission/AppCustomPermissionEvaluator.kt
+++ web-security-dummy-project/src/main/kotlin/io/positivinh/virtuoso/web/security/dummy/permission/AppCustomPermissionEvaluator.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 import java.io.Serializable
 
 @Component("appCustomPermissionEvaluator")
-class AppCustomerPermissionEvaluator : PermissionEvaluator {
+class AppCustomPermissionEvaluator : PermissionEvaluator {
 
     override fun hasPermission(
         authentication: Authentication?,
